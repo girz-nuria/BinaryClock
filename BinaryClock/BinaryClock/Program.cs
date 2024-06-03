70bb85a45de5112978d642a73cd2122a8556f665
Nuria Gîrz <156543261+girz-nuria@users.noreply.github.com>
2024-06-03 15:13:38 +0300

    Add files via upload

---
+++ BinaryClock/BinaryClock/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace binary_clock
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Introduceti ora sub forma HH:MM:SS :");
+            string ora = Console.ReadLine();
+
+            int linii = 4;
+            int coloane = 6;
+
+            int[,] m = new int[linii, coloane];
+
+
+            for (int j = 0; j < coloane; j++)
+            {
+                int k = j;
+                if (j > 1)
+                    k++;
+                if (j > 3)
+                    k++;
+                int nr = Convert.ToInt32(ora[k]);
+                for (int i = linii - 1; i >= 0; i--)
+                {
+                    int r = nr % 2;
+                    m[i, j] = r;
+                    nr = nr / 2;
+
+                }
+            }
+
+
+            Console.WriteLine("Ceasul Binar:");
+
+            for (int i = 0; i < linii; i++)
+            {
+                for (int j = 0; j < coloane; j++)
+                {
+                    if ((i == 0 && j == 0) || (i == 1 && j == 0) || (i == 0 && j == 2) || (i == 0 && j == 4))
+                        Console.Write("  ");
+                    else
+                        Console.Write(m[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.ReadKey();
+        }
+    }
+}
